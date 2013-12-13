c32853ac9c3dd38341492761cd8a5fe95bec9a89
Adam Lusted <adam@etoxin.net>
2013-12-13 12:25:31 +1100

    changed name

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", function(req, res){
 var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function (socket) {
-	socket.emit('message', { message: 'welcome to Hell.' });
+	socket.emit('message', { message: 'welcome to Facederp.me' });
 	socket.on('send', function (data) {
 		io.sockets.emit('message', data);
 	});
